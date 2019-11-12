3ffc3b0ceea8b5b0329112dc4f1170b7b8d41e54
Migle Kubiliute <migle.kubiliute@gmail.com>
2019-11-12 16:27:59 +0000

    final tweeks

---
+++ src/Javabuzz.js
@@ -0,0 +1,36 @@
+var Javabuzz = function() {};
+
+Javabuzz.prototype._isDivisibleBy = function(number, divisor) {
+    return (number % divisor === 0);
+    };
+
+Javabuzz.prototype.isDivisibleByThree = function(number) {
+    return (number % 3 === 0);
+    };
+
+Javabuzz.prototype.isDivisibleByFifteen = function(number) {
+return this._isDivisibleBy(number, 15);
+    };
+
+Javabuzz.prototype.isDivisibleByFive = function(number)  {
+    return this._isDivisibleBy(number, 5);
+    };
+Javabuzz.prototype.says = function (number) {
+    if (this.isDivisibleByFifteen(number)) {
+        return "Javabuzz";
+    }
+    if (this.isDivisibleByThree(number)) {
+        return "Java";
+    }
+    if (this.isDivisibleByFive(number)) {
+        return "Buzz";
+    }
+    return number;
+};
+
+
+
+
+
+
+
